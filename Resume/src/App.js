231e591e1676102da332c1e12403aaee22281a41
unknown <hemangichavda59@gmail.com>
2024-03-29 09:48:09 +0530

    Resume

---
+++ Resume/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Resume1 from './Componets/Resume1';
+
+function App() {
+  return (
+    <>
+    <div>
+    <Resume1/>
+    </div>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ Resume/src/Componets/Resume1.jsx
@@ -0,0 +1,256 @@
+import React from 'react'
+import logo from './image/logo.jpg';
+import star from './image/star.png';
+import call from './image/call.jpg';
+
+export default function Resume1() {
+  return (
+    <>
+    <div></div>
+      <div>
+    <h1>Chavda Hemangi</h1>
+    </div>
+    <p style={{"marginLeft":"5px","width":"30%","fontWeight":"bold"}} >Full Stack Devloper</p>
+   <div style={{"width":"30%","float":"left"}} >
+  
+   
+      
+        <div style={{"display":"flex"}}  >
+
+            <div><img src={logo} style={{"height":"50px" ,"position":"absolute" ,"left":""}}  /> <p style={{"position":" relative" ,"left":"40px"}}>+91826648987</p>
+             </div>
+            {/* <div><p style={{"marginLeft":"30px"}}>  andrew.clerk.jj@gmail.com github.io/andrew-clerk-jj  san  Fransico,CA</p>
+</div> */}
+      
+      </div>
+
+      <div style={{"borderRight":"2px solid ", "width":"90%"}}>
+       <h2 > Skills:</h2>
+        
+
+        
+        <h1 style={{"color":"purple"}}>Client-Side</h1>
+       
+       <p style={{"display":"flex","gap":"15px"}}>
+           <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>HTML </p>
+           <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>CSS </p>
+           <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>JS </p>
+          
+          <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>Angular </p>
+          </p>
+
+       <p style={{"display":"flex","gap":"15px"}}>
+           <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>React </p>
+          
+        
+          <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>Vue </p>
+          
+          <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>Redux</p>
+          
+          <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>Typescript</p>
+       </p>
+
+       <p style={{"display":"flex","gap":"15px"}}>
+           <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>bootsrap </p>
+          
+        
+          <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>Backbone </p>
+          
+         
+       </p>
+
+       </div>
+        
+       <div style={{"borderRight":"2px solid ", "width":"90%"}}>
+
+        <h1 style={{"color":"purple"}}>Server-Side</h1>
+       
+        <p style={{"display":"flex","gap":"15px"}}>
+           <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>Python </p>
+          
+        
+          <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>Numpy </p>
+          
+          <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>scipy </p>
+          
+          <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>Scikit-learn </p>
+       </p>
+
+       <p style={{"display":"flex","gap":"15px"}}>
+           <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>Keras </p>
+          
+        
+          <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>Eli5 </p>
+          
+          <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>TensorFlow</p>
+          
+          <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>MySql</p>
+       </p>
+
+       <p style={{"display":"flex","gap":"15px"}}>
+           <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>NodeJs </p>
+          
+        
+          <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>Redis </p>
+          
+         
+       </p>
+       </div>
+       <div style={{"borderRight":"2px solid ", "width":"90%"}}>
+
+     
+       <h1 style={{"color":"purple"}}>Devlopement & Operation</h1>
+       
+       <p style={{"display":"flex","gap":"15px"}}>
+          <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>JUnit </p>
+         
+       
+         <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>NUnit</p>
+         
+         <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>PyUnit </p>
+         
+         <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>TestCafe </p>
+      </p>
+
+      <p style={{"display":"flex","gap":"15px"}}>
+          <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>Mocha </p>
+         
+       
+         <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>GIT </p>
+         
+         <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>Jest</p>
+         
+         <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>Scrum</p>
+      </p>
+
+      <p style={{"display":"flex","gap":"15px"}}>
+          <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>Agite </p>
+         
+       
+         <p style={{"borderBottom":"2px solid black","width":"","display":"inline" }}>Azure Devops </p>
+         
+        
+      </p>
+      </div>
+      <div style={{"borderRight":"2px solid ", "width":"90%"}}>
+       <h2 style={{"color":"black"}}>CERTIFICATES</h2>
+       <hr />
+       <h2>AWS Machine Learning - Professinal </h2>
+       <p>Amozon web service,2018</p>
+
+       <h2>Certified Agile Practitioner </h2>
+       <p>PMI,2015</p>
+
+
+       <h2>AWS Solution Architect-Associate </h2>
+       <p>Amozon web service,2013</p>
+
+       <h2>Java Devlopement Certified  Professional</h2>
+       <p>Oracle,2010</p>
+
+       <h1 style={{"color":"purple"}}>PROJECTS</h1>
+        <hr/>
+        <p>OpenFlow Based Firewell</p>
+
+
+
+
+
+      
+        </div>
+
+       
+
+       
+
+
+
+
+
+   
+        
+
+        {/* <hr /> */}
+       
+        
+       
+       
+      
+
+   
+   </div>
+   <div style={{"width":"70%","float":"left"}}>
+        <h1 style={{"color":"purple"}}>SUMMERY</h1>
+        <hr/>
+        <p>Full Stack Devloper with OVer 10 years of exprience in javajs,Angular,Vue,React,python,Numpy,Scipy,Scikit,Led devlopement of $500k reserach project which was deemad a "gold standard" by the client.
+            incressed client's revenue 2-fold after fine-tuning AL/ML -based
+            algorithms,well-acquinted with HR methodologies.
+        </p>
+        <h1 style={{"color":"purple"}}>EDUCATION</h1>
+        <hr/>
+        <h4>M.S in Computer Science </h4>
+        <p style={{"color":"navy"}}> Standard University</p>
+
+        <h1 style={{"color":"purple"}}>EXPERINCE</h1>
+        <hr/>
+        <h4>Senior Full Stack Devloper</h4>
+        <p>Boyle</p>
+        <p>Boyle is an international Technology and Management Consulting Group With a rapid Practitioner
+            devlopement and innovative solution for demanding projects,I work as a full-stack-devloper
+            here,responsible for building deploying and maintaining internal web application.
+          
+        </p>
+        hired,trained and leading an agile team of 7 full-stack devlopers
+        devloped indexed database architecture using SQL procedure and trigger for 10 different application
+        worked with core java to devlop automated solution to include web interface using html,css,javascript and web services.
+
+        <h1 style={{"color":"purple"}}>Full Stack Developer</h1>
+        <h4 style={{"color":"navy"}}>Lauzon</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus asperiores quo culpa officia? Reiciendis non hic minima quo inventore, amet repudiandae rem assumenda? Mollitia odio non consectetur maxime consequuntur! Voluptatem molestias hic magni voluptatum consequuntur dolorum praesentium ab quisquam doloribus ut ex quas culpa soluta, eaque sequi perferendis recusandae alias libero deserunt magnam ipsam provident nam, minus optio? Modi quas maxime nam facilis possimus voluptates incidunt optio et voluptate, natus ullam ratione aspernatur doloribus impedit aperiam, laboriosam eveniet hic voluptatem iure minus illum asperiores. Sit dicta, nisi officiis consectetur pariatur quas laborum sint dolores. A non quibusdam tempora fugiat soluta!</p>
+
+        <h1>Solution Architect</h1>
+        <h4 style={{"color":"navy"}}>Keeling Group</h4>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit blanditiis explicabo tempora expedita porro eligendi eveniet veritatis sunt nostrum? Beatae consequatur nam perferendis, ipsam est culpa eos nihil aperiam asperiores soluta delectus commodi veniam quis quod deleniti vitae dolor dignissimos odio nulla. Velit exercitationem enim assumenda eveniet placeat culpa saepe fuga, molestias iste facere maiores asperiores, sint quibusdam cumque vitae, doloremque dicta incidunt voluptas ea eius a dolorem! Velit iste nisi dolores iure! Sequi, ratione? Blanditiis aut amet animi praesentium! Expedita beatae libero rerum quibusdam cumque error unde nemo consequatur excepturi deserunt. Aspernatur, vero voluptate? Praesentium aut architecto dolorem quisquam?
+
+        </p>
+        
+        <h1 style={{"color":"purple"}}>VOLUNTEERING</h1>
+        <hr/>
+        <h4 style={{"color":"navy"}}>Volunteer</h4>
+        <h4 style={{"color":"purple"}}>Childrens Autuism fund</h4>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid expedita veritatis exercitationem sed eveniet non totam quisquam animi asperiores, hic laboriosam aperiam minus. Doloribus in odit consectetur autem officiis itaque voluptas tempore possimus distinctio exercitationem labore maiores nemo praesentium vitae perspiciatis velit natus, cum ratione consequatur. Vel, rerum recusandae? Officiis corrupti laborum mollitia repellat nam eum eligendi dignissimos. Similique temporibus perferendis cupiditate veritatis officia earum libero non eveniet tempore enim, voluptatem blanditiis debitis harum neque totam dolore nemo, incidunt nam. Quod vitae aut quos mollitia aliquam eligendi dolore nam unde quae provident? Distinctio exercitationem, neque ipsam veniam fuga molestiae quidem!
+
+        </p>
+
+        <h1>STRENGTHS</h1>
+        <hr/>
+        <div >
+        <img src={star} style={{"height":"35px" ,"marginTop":"35px" ,"borderRadius":"50%"}} />
+        <br/>
+        <img src={call} style={{"height":"35px" ,"marginTop":"15px","borderRadius":"50%" ,"left":""}} />
+        
+        </div>
+    
+       
+        
+       
+
+
+
+
+
+
+
+
+
+
+       
+
+
+   </div>
+   </>
+  )
+}
